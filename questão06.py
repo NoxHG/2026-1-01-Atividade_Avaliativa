25f885cfcf55ab8360a7eb3b1baf23837cfc77d3
Luiz Heytor do Nascimento <157654636+NoxHG@users.noreply.github.com>
2026-03-27 09:52:48 -0300

    Create questão06.py

---
+++ questão06.py
@@ -0,0 +1,9 @@
+#QUESTÂO6
+print('Por favor, insira 4 numeros, dando enter a cada um', 'Exemplo de formato:', 1,2,3,4,5, sep='\n')
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(f'a soma dos números é: {a+b+c+d}')
+print(f'o produto dos números é: {a * b * c * d}')
