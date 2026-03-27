4d4fc2e2f4cc5a786855790036d9195323f93eb3
Luiz Heytor do Nascimento <157654636+NoxHG@users.noreply.github.com>
2026-03-27 09:52:30 -0300

    Create questão05.py

---
+++ questão05.py
@@ -0,0 +1,6 @@
+#QUESTÃO5
+print('Olá! insira seu nome, por favor.')
+nome = input()
+print(f'Muito bem {nome}, agora insira o seu curso.')
+curso = input()
+print(f'Ótimo! Bem vindo(a) {nome} de {curso}.')
