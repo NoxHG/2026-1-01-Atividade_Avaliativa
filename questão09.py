c8b69bd02f0f7db319b2ae5c6aa1bb5968000173
Luiz Heytor do Nascimento <157654636+NoxHG@users.noreply.github.com>
2026-03-27 09:53:40 -0300

    Create questão09.py

---
+++ questão09.py
@@ -0,0 +1,14 @@
+#QUESTÃO9
+n1 = int(input('Insira um número: '))
+n2 = int(input('Insira um número: '))
+n3 = int(input('Insira um número: '))
+n4 = int(input('Insira um número: '))
+
+print(f'o produto dos 4 números é: {(n1 * n2 * n3 * n4)}')
+
+if (n1 * n2 * n3 * n4) > 0:
+    print('o produto é positivo')
+if (n1 * n2 * n3 * n4) < 0:
+    print('o produto não é positivo.')
+
+#Gastei uns neurônios nesse ngc aq
