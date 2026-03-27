d93fdeb039eb0c983144e36724fdcf0920e7339f
Luiz Heytor do Nascimento <157654636+NoxHG@users.noreply.github.com>
2026-03-27 09:50:46 -0300

    Create questão4.py

---
+++ questão4.py
@@ -0,0 +1,7 @@
+#QUESTÂO4
+print('Olá! por favor, insira seu nome.')
+nome = input()
+print('Ótimo! agora insira sua cidade.')
+cidade = input()
+print('Perfeito!')
+print(f'Olá, {nome}, você mora em {cidade}. Seja muito bem vinda(o).')
