c294f24143a9321ad30e7c5212177f12372f0af8
Luiz Heytor do Nascimento <157654636+NoxHG@users.noreply.github.com>
2026-03-27 09:51:04 -0300

    Update Questão1-3.py

--- Questão1-3.py
+++ Questão1-3.py
@@ -1,8 +1,2 @@
 #QUESTÃO1
 print('Bom dia família infoweb 2026!')
-
-#QUESTÃO2
-print('Meu nome é Fulano.', 'Tenho 18 anos.', 'Estou no curso de Informática para Internet.', sep='\n')
-
-#QUESTÂO3
-print('***CARDÁPIO***', '1.Pizza', '2.Hambúrguer', '3.Suco', '***Bom Apetite!***', sep='\n')
